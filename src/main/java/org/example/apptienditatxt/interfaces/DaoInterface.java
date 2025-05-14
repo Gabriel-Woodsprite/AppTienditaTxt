77acb587cd66af35429a5c5bce84e6c7540798b8
Gabriel-Woodsprite <L23650343@zitacuaro.tecnm.mx>
2025-05-13 18:06:10 -0600

    DAO productos | Plantilla User Message

--- src/main/java/org/example/apptienditatxt/interfaces/DaoInterface.java
+++ src/main/java/org/example/apptienditatxt/interfaces/DaoInterface.java
@@ -1,11 +1,13 @@
 package org.example.apptienditatxt.interfaces;
 
+import org.example.apptienditatxt.model.Product;
+
 import java.util.List;
 
-public interface DaoInterface<Item, ID> {
-	void createItem();
-	Item readItem(int itemId);
-	List<Item> readAllItems();
+public interface DaoInterface {
+	void createItem(String path, List<Product> productos) throws Exception;
+	Product readItem(String path);
+	List<Product> readAllItems(String path);
 	void updateItem();
 	void deleteItem();
 }
--- src/main/java/org/example/apptienditatxt/model/Product.java
+++ src/main/java/org/example/apptienditatxt/model/Product.java
@@ -1,4 +1,146 @@
 package org.example.apptienditatxt.model;
 
 public class Product {
+	private String barcode;
+	private String name;
+	private String brand;
+	private String category;
+	private String content;
+	private String presentacion;
+	private String measurementUnit;
+	private boolean active;
+	private String description;
+	private String price;
+	private String image;
+
+	public Product(String barcode) {
+		this.barcode = barcode;
+	}
+	public Product() {}
+
+	///////////////////
+	//////GETTERS//////
+	///////////////////
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getPresentacion() {
+		return presentacion;
+	}
+
+	public String getMeasurementUnit() {
+		return measurementUnit;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	///////////////////
+	//////SETTERS//////
+	///////////////////
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+
+	public void setMeasurementUnit(String measurementUnit) {
+		this.measurementUnit = measurementUnit;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	///////////////////
+	//////OVERRIDES////
+	///////////////////
+	@Override
+	public String toString() {
+		return "Product{" +
+				"barcode='" + barcode + '\'' +
+				", name='" + name + '\'' +
+				", brand='" + brand + '\'' +
+				", category='" + category + '\'' +
+				", content='" + content + '\'' +
+				", presentacion='" + presentacion + '\'' +
+				", measurementUnit='" + measurementUnit + '\'' +
+				", active=" + active +
+				", description='" + description + '\'' +
+				", price='" + price + '\'' +
+				", image='" + image + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return false;
+
+		if(obj instanceof Product product) {
+			return product.getBarcode().equals(this.getBarcode());
+		}
+		return false;
+	}
 }
