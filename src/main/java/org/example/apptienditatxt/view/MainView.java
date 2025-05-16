9a3e830eba342bef193ce3baa6dce94ffa651d7f
Musulini <spidermanjugandominecraft@gmail.com>
2025-05-16 00:29:54 -0600

    catalogo view

--- src/main/java/org/example/apptienditatxt/view/MainView.java
+++ src/main/java/org/example/apptienditatxt/view/MainView.java
@@ -3,17 +3,29 @@ package org.example.apptienditatxt.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainView extends Application {
+
+    double screenWidth = Screen.getPrimary().getBounds().getWidth();
+    double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
     @Override
     public void start(Stage stage) throws IOException {
+
+        System.out.println("stage = " + stage);
+
+        /*___FXML___*/
         FXMLLoader fxmlLoader = new FXMLLoader(MainView.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        /*___WINDOW___*/
+        Scene scene = new Scene(fxmlLoader.load(), 1536, 864);
+        stage.setTitle("Gabo's Store");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
